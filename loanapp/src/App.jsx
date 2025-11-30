e8699c6cc5bd9e4591b45285afb1c70d610c1eb0
Amrik Ghosh <amrikghosh20@gmail.com>
2025-11-30 22:19:45 +0530

    feat: updated the latest fixes

--- loanapp/src/App.jsx
+++ loanapp/src/App.jsx
@@ -29,6 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <h1>This is amrik first ever deplopyment</h1>
+      <h1>Made the updated deployemnt</h1>
     </>
   )
 }
